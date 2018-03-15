c4c91b896a9b05f80d3daebec8f2febdd4e7e44c
Ayman Gassara <a.gassara@root.sa>
2018-03-15 08:53:11 +0100

    Add project files.

---
+++ MenuProject/MenuProject/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MenuProject
+{
+	public partial class MainPage : MasterDetailPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+
+            ListView1.ItemsSource = new List<Menu>
+            {
+                     new Menu{ Name ="My ACCOUNT"},
+                     new Menu{ Name ="SHOP"},
+                     new Menu{ Name ="FAQ"},
+                     new Menu{ Name ="NOTIFICATIONS",Status = "2"}
+
+        };
+        }
+        private void LV1(object sender, SelectedItemChangedEventArgs e)
+        {
+            var Item = e.SelectedItem as Menu;
+
+            if (Item.Name.Equals("My ACCOUNT"))
+            {
+                Detail = new AccountPage();
+                IsPresented = false;
+            }
+            else
+               if (Item.Name.Equals("SHOP"))
+            {
+                Detail = new ShopPage();
+                IsPresented = false;
+            }
+            else
+               if (Item.Name.Equals("FAQ"))
+            {
+                Detail = new FAQPage();
+                IsPresented = false;
+            }
+            else
+               if (Item.Name.Equals("NOTIFICATIONS"))
+            {
+                Detail = new NotificationPage();
+                IsPresented = false;
+            }
+
+        }
+
+    }
+}
---
+++ MenuProject/MenuProject/Menu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MenuProject
+{
+    public class Menu
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
